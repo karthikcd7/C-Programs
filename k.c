8467c400668362fcf38ec098033fa1e456767cb8
karthikcd7 <42541700+karthikcd7@users.noreply.github.com>
2019-01-25 18:04:35 +0530

    Update k.c

--- k.c
+++ k.c
@@ -1,63 +1,60 @@
 #include<stdio.h>
-#include<complex.h>
 #include<math.h>
-typedef struct root
+typedef struct complex
 {
-float r1;
-float r2;
-}num;
+float real;
+float imag;
+}COMPLEX;
 void input(float *a,float *b,float *c)
 {
 printf("enter a,b,c\n");
-scanf("%f%f%f ",a,b,c);
+scanf("%f%f%f",a,b,c);
 
 }
-void compute(float *d,float a,float b,float c,num *r)
+void compute(float *d,float a,float b,float c,COMPLEX *r1,COMPLEX *r2)
 {
 *d=((b*b)-(4*a*c));
 if(*d==0)
 {
-r->r1=-b/2*a;
+r1->real=-b/2*a;
 }
 else if(*d>0)
 {
-r->r1=(-b+sqrt(*d))/(2*a);
-r->r2=(-b-sqrt(*d))/(2*a);
+r1->real=(-b+ sqrt(*d))/(2*a);
+r2->real=(-b- sqrt(*d))/(2*a);
 }
 else
 {
-r->r1=(b/(2*a));
-r->r2=((*d)/(2*a));
+r1->real=r2->real=(b/(2*a));
+r1->imag=r2->imag=((*d)/(2*a));
 }
 }
-void output(float *d,float a,float b,float c,num *r)
+void output(float *d,float a,float b,float c,COMPLEX *r1, COMPLEX *r2)
 {
 if(*d==0)
 {
 printf("\nsolution is real and equal\n");
-printf("%f",r->r1);
+printf("%f",r1->real);
 }
 else if(*d>0)
 {
 printf("\nsolution is real and distinct\n");
-printf("%f and %f",r->r1,r->r2);
+printf("%f and %f",r1->real,r2->real);
 }
 else
 {
 printf("\nsolution is imaginary\n");
-printf("%f + (%f)i and %f - (%f)i",r->r1,r->r2,r->r1,r->r2);
+printf("%f + (%f)i and %f - (%f)i",r1->real,r1->imag,r2->real,r2->imag);
 }
 }
 void main()
 {
 float a,b,c,d;
-num r;
-
+COMPLEX r1,r2;
 input(&a,&b,&c);
-compute(&d,a,b,c,&r);
-output(&d,a,b,c,&r);
+compute(&d,a,b,c,&r1,&r2);
+output(&d,a,b,c,&r1,&r2);
 }
-
 
 
 
